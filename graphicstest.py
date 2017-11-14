0798c249f521f6fcfae31c7cd513437640d60890
boochow <boochowp@gmail.com>
2017-11-14 23:07:13 +0900

    Update graphicstest.py
    replaced [ ] to ( )

--- graphicstest.py
+++ graphicstest.py
@@ -11,56 +11,56 @@ tft.rgb(True)
 def testlines(color):
     tft.fill(TFT.BLACK)
     for x in range(0, tft.size()[0], 6):
-        tft.line([0,0],[x, tft.size()[1] - 1], color)
+        tft.line((0,0),(x, tft.size()[1] - 1), color)
     for y in range(0, tft.size()[1], 6):
-        tft.line([0,0],[tft.size()[0] - 1,y], color)
+        tft.line((0,0),(tft.size()[0] - 1, y), color)
 
     tft.fill(TFT.BLACK)
     for x in range(0, tft.size()[0], 6):
-        tft.line([tft.size()[0] - 1, 0], [x, tft.size()[1] - 1], color)
+        tft.line((tft.size()[0] - 1, 0), (x, tft.size()[1] - 1), color)
     for y in range(0, tft.size()[1], 6):
-        tft.line([tft.size()[0] - 1, 0], [0, y], color)
+        tft.line((tft.size()[0] - 1, 0), (0, y), color)
 
     tft.fill(TFT.BLACK)
     for x in range(0, tft.size()[0], 6):
-        tft.line([0, tft.size()[1] - 1], [x, 0], color)
+        tft.line((0, tft.size()[1] - 1), (x, 0), color)
     for y in range(0, tft.size()[1], 6):
-        tft.line([0, tft.size()[1] - 1], [tft.size()[0] - 1,y], color)
+        tft.line((0, tft.size()[1] - 1), (tft.size()[0] - 1,y), color)
 
     tft.fill(TFT.BLACK)
     for x in range(0, tft.size()[0], 6):
-        tft.line([tft.size()[0] - 1, tft.size()[1] - 1], [x, 0], color)
+        tft.line((tft.size()[0] - 1, tft.size()[1] - 1), (x, 0), color)
     for y in range(0, tft.size()[1], 6):
-        tft.line([tft.size()[0] - 1, tft.size()[1] - 1], [0, y], color)
+        tft.line((tft.size()[0] - 1, tft.size()[1] - 1), (0, y), color)
 
 def testfastlines(color1, color2):
     tft.fill(TFT.BLACK)
     for y in range(0, tft.size()[1], 5):
-        tft.hline([0,y], tft.size()[0], color1)
+        tft.hline((0,y), tft.size()[0], color1)
     for x in range(0, tft.size()[0], 5):
-        tft.vline([x,0], tft.size()[1], color2)
+        tft.vline((x,0), tft.size()[1], color2)
 
 def testdrawrects(color):
     tft.fill(TFT.BLACK);
     for x in range(0,tft.size()[0],6):
-        tft.rect([tft.size()[0]//2 - x//2, tft.size()[1]//2 - x/2], [x,x], color)
+        tft.rect((tft.size()[0]//2 - x//2, tft.size()[1]//2 - x/2), (x, x), color)
 
 def testfillrects(color1, color2):
     tft.fill(TFT.BLACK);
     for x in range(tft.size()[0],0,-6):
-        tft.fillrect([tft.size()[0]//2 - x//2, tft.size()[1]//2 - x/2], [x,x], color1)
-        tft.rect([tft.size()[0]//2 - x//2, tft.size()[1]//2 - x/2], [x,x], color2)
+        tft.fillrect((tft.size()[0]//2 - x//2, tft.size()[1]//2 - x/2), (x, x), color1)
+        tft.rect((tft.size()[0]//2 - x//2, tft.size()[1]//2 - x/2), (x, x), color2)
 
 
 def testfillcircles(radius, color):
     for x in range(radius, tft.size()[0], radius * 2):
         for y in range(radius, tft.size()[1], radius * 2):
-            tft.fillcircle([x, y], radius, color)
+            tft.fillcircle((x, y), radius, color)
 
 def testdrawcircles(radius, color):
     for x in range(0, tft.size()[0] + radius, radius * 2):
         for y in range(0, tft.size()[1] + radius, radius * 2):
-            tft.circle([x, y], radius, color)
+            tft.circle((x, y), radius, color)
 
 def testtriangles():
     tft.fill(TFT.BLACK);
@@ -70,9 +70,9 @@ def testtriangles():
     y = 0
     z = tft.size()[0]
     for t in range(0, 15):
-        tft.line([w, y], [y, x], color)
-        tft.line([y, x], [z, x], color)
-        tft.line([z, x], [w, y], color)
+        tft.line((w, y), (y, x), color)
+        tft.line((y, x), (z, x), color)
+        tft.line((z, x), (w, y), color)
         x -= 4
         y += 4
         z -= 4
@@ -87,7 +87,7 @@ def testroundrects():
         w = tft.size()[0] - 2
         h = tft.size()[1] - 2
         for i in range(17):
-            tft.rect([x, y], [w, h], color)
+            tft.rect((x, y), (w, h), color)
             x += 2
             y += 3
             w -= 4
